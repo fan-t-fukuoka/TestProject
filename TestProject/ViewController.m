33f070aa67a5c24d11d09d94f071c5bd5f01cd0b
fan-t-fukuoka <t_fukuoka@fancs.com>
2015-01-13 16:17:14 +0900

    Push TestProject.

---
+++ TestProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestProject
+//
+//  Created by fan-t-fukuoka on 2015/01/13.
+//  Copyright (c) 2015年 F@N Communications. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"Hello world");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
